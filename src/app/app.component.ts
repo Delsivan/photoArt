cdc9a19b2f4ceda428a8473d57ce3bd50c2e00ec
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-06-30 09:36:49 -0300

    Diretiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  photos = [
+    {
+      url:
+      'https://images.unsplash.com/photo-1624983918570-6f2223069c0c?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+      description: 'Gato'
+    },
+    {
+      url:
+      'https://images.unsplash.com/photo-1506903536293-8419385acdce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=282&q=80',
+      description: 'Cachorro'
+    }
+];
   
 }
