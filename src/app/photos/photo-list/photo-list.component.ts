b2407d8153edae89bb8248a97d36383bb72ceeca
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-07-07 23:21:58 -0300

    Resolvers

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { IPhoto } from 'src/app/models/IPhoto';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -14,21 +14,11 @@ export class PhotoListComponent implements OnInit {
   filter: string = '';
 
   
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute,
-    ) { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.photos = this.activatedRoute.snapshot.data['photos'];
 
-      const userName = this.activatedRoute
-      .snapshot
-      .params
-      .userName;
-
-      this.photoService
-        .listFromUser(userName)
-        .subscribe(photos => this.photos = photos);
   }
   onKeyUp(target : any) {
     if(target instanceof EventTarget) {
