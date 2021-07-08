a9ac4f871afdbe66c09a19363e67ad89414b0d03
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-07-08 17:13:10 -0300

    Paginação de dados

--- src/app/photos/photo-list/photo-list.resolver.ts
+++ src/app/photos/photo-list/photo-list.resolver.ts
@@ -13,6 +13,6 @@ export class PhotoListResolver implements Resolve<Observable<IPhoto[]>>{
     }
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IPhoto[]> {
         const userName = route.params.userName;
-        return this.service.listFromUser(userName);
+        return this.service.listFromUserPaginated(userName, 1);
     }
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { IPhoto } from "src/app/models/IPhoto";
 
@@ -17,6 +17,15 @@ export class PhotoService {
 
         return this.http
             .get<IPhoto[]>(API + '/' + userName + '/photos');
+        
+    }
+
+    listFromUserPaginated(userName: string, page: number) {
+        const params = new HttpParams()
+            .append('page', page.toString());
+        
+        return this.http
+            .get<IPhoto[]>(API + '/' + userName + '/photos', { params: params });
         
     }
 }
